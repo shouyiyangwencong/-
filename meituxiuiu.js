cf94dc9debc4683132db061bbd872edda1578737
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-01-20 23:56:49 +0800

    Update meituxiuiu.js

--- meituxiuiu.js
+++ meituxiuiu.js
@@ -2,7 +2,7 @@
 圈X配置：
 
 [rewrite_local]
-^https:\/\/api\.xiuxiu\.meitu\.com\/v1\/user\/show\.json$ url script-response-body https://raw.githubusercontent.com/shouyiyangwencong/-/main/meituxiuiu.js
+^https:\/\/api\.xiuxiu\.meitu\.com\/v1\/user\/show\.json url script-response-body https://raw.githubusercontent.com/shouyiyangwencong/-/main/meituxiuiu.js
 
 [mitm]
 hostname = api.xiuxiu.meitu.com
