0a61a11f1abab5eac125028a65797095196585a7
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-01-20 22:51:41 +0800

    Update meituxiuiu.js

--- meituxiuiu.js
+++ meituxiuiu.js
@@ -1,7 +1,22 @@
-hostname＝*
 
-#美图秀秀 解锁限制 A+
-https://api\.xiuxiu\.meitu\.com/v1/user/show.json.+ url script-response-body xxxx.js
+圈X配置：
 
-body = $response.body.replace(/"vip_type":0/g, '"vip_type":1');
+[rewrite_local]
+^https:\/\/api\\.xiuxiu\\.meitu\\.com\/v1\/user\/show.json$ url script-response-body https://raw.githubusercontent.com/shouyiyangwencong/-/main/meituxiuiu.js
+
+[mitm]
+hostname = api.xiuxiu.meitu.com
+
+*/var body = $response.body;
+var url = $request.url;
+
+const path1 = "\/v1\/user\/show.json";
+
+let obj = JSON.parse(body);
+
+if (url.indexOf(path1) != -1) {
+	obj.data["vip_type"] = 1;
+	body = JSON.stringify(obj);  
+ }
+
 $done({body});
