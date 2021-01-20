c5cc89886c10e569610049ff391c1a882a5130e4
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-01-20 23:18:38 +0800

    Update meituxiuiu.js

--- meituxiuiu.js
+++ meituxiuiu.js
@@ -2,13 +2,13 @@
 圈X配置：
 
 [rewrite_local]
-^https://api\.xiuxiu\.meitu\.com/v1/user/show.json.+ url script-response-body https://raw.githubusercontent.com/shouyiyangwencong/-/main/meituxiuiu.js
+^https:\/\/api.xiuxiu.meitu.com\/v1\/user\/show.json$ url script-response-body https://raw.githubusercontent.com/shouyiyangwencong/-/main/meituxiuiu.js
 
 [mitm]
 hostname = api.xiuxiu.meitu.com
 
 */
 
-var body = $response.body;
-body = $response.body.replace(/"vip_type":0/g, '"vip_type":1');
+var body = $response.body; //声明一个变量body并以响应消息体赋值
+body=body.replacebody=body.replace(/"vip_type":0/g, '"vip_type":1');
 $done({body});
