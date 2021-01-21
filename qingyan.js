768326cf75d38781629a2790e2bbb8a2e1c5fdc5
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-01-21 10:59:27 +0800

    Update qingyan.js

--- qingyan.js
+++ qingyan.js
@@ -20,4 +20,4 @@ function key() {
  $done({body:JSON.stringify(obj)});
 }
 // 最终只需用户在结尾按格式录入key("键1@键2……", [值1，值2……])
-key("flag@end_time@start_time", [true, 1639446065, 1605316215])
+key("flag@"end_time"@"start_time"", [true, 1639446065, 1605316215])
