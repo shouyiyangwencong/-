8fdeb5081e516d98778d14f9fabe7fd4aa690c0b
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-01-24 13:44:53 +0800

    Update pdasf.js

--- pdasf.js
+++ pdasf.js
@@ -4,7 +4,7 @@
  * 
  */
 
-const url = `https://www.pdawiki.com/forum/plugin.php?id=are_yqs_money:spend&typeid=1&formhash=3b76551f`;
+const url = `https://www.pdawiki.com/forum/plugin.php?id=are_yqs_money:spend&typeid=2&formhash=3b76551f`;
 const method = `GET`;
 const headers = {
 'Cookie' : `usoK_2132_connect_is_bind=0; usoK_2132_lastact=1611413123%09misc.php%09patch; Hm_lpvt_346381cad752cdb294c1dd3544d1d65a=1611413124; Hm_lvt_346381cad752cdb294c1dd3544d1d65a=1611114577,1611116043,1611159364,1611410162; _ga=GA1.2.1334470196.1610409781; _gid=GA1.2.735512033.1611410164; usoK_2132_checkpm=1; usoK_2132_noticeTitle=1; usoK_2132_sid=mhgqCc; _gat_gtag_UA_120281465_1=1; usoK_2132_sendmail=1; usoK_2132_lip=223.91.28.160%2C1611412935; usoK_2132_onlineusernum=595; usoK_2132_ulastactivity=fc64dxGf65kBgcV9n6A3%2BeuXETyrG8eAJqEByoW9kiKA74vZm4ji; usoK_2132_nofavfid=1; usoK_2132_auth=fa07c2amEQWJaoXE2PtjrvJ7KrEozBMLQSPP2rOhMh%2BLBLZZGwH6vYiM6TypS%2F3q1btcQ1nxLZgi%2F%2FgaO3XB6NlXsMw; usoK_2132_lastcheckfeed=335060%7C1611075046; usoK_2132_lastvisit=1611071358; usoK_2132_saltkey=akh19hi2`,
