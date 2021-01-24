bedb5084d8646376dbd30b81992a84caef4393d0
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-01-24 14:38:03 +0800

    Update qingyan.js

--- qingyan.js
+++ qingyan.js
@@ -1,19 +1,9 @@
 
-re('"flag":\\w+@"end_time":\\d+@"start_time":\\d+','"flag":true@"end_time":1639446065@"start_time":1605316215')
+*/
 
-function re() {
- var body = $response.body;
- if (arguments[0].includes("@")) {
-  var regs = arguments[0].split("@");
-  var strs = arguments[1].split("@");
-  for (i = 0;i < regs.length;i++) {
-   var reg = new RegExp(regs[i],"g");
-   body = body.replace(reg, strs[i]);
- }
-}
- else {
-  var reg = new RegExp(arguments[0],"g");
-  body = body.replace(reg, arguments[1]);
-}
- $done(body);
-} 
+var obj = JSON.parse($response.body);
+obj.data.flag = true;
+obj.data.start_time = "1605316215";
+obj.data.end_time = "1639446065";
+
+$done({body: JSON.stringify(obj)}); 
