940efecb04529940bb6f45e42c015479765d2598
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-02-08 15:13:26 +0800

    Update pdadh.js

--- pdadh.js
+++ pdadh.js
@@ -26,14 +26,10 @@ const myRequest = {
 };
 
 $task.fetch(myRequest).then(response => {  
-  if(response.body.indexOf("恭喜") != -1){
+  if(response.body.indexOf("恭喜")&&("跳转") != -1){
     $notify("pdawiki兑换米粒", "", "成功[庆祝]")
-  }if(response.body.indexOf("还差") != -1){
+  }if(response.body.indexOf("还差")&& ("返回") != -1){
     $notify("pdawiki兑换米粒", "失败👇🏻", "果实暂时不够🤞")
-  }if(response.body.indexOf("跳转") != -1){
-    $notify("pdawiki兑换米粒", "", "自动跳转成功!")
-  }if(response.body.indexOf("返回") != -1){
-    $notify("pdawiki兑换米粒", "", "自动返回成功!")
   }else {
     $notify("pdawiki兑换米粒", "失败👇", "可能要更新一下请求头信息了")
   }  
