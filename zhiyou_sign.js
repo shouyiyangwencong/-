dffcfddccc97a7924fb4a89212197d9e34fbd458
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-02-16 00:15:53 +0800

    Update zhiyou_sign.js

--- zhiyou_sign.js
+++ zhiyou_sign.js
@@ -4,7 +4,7 @@
  * 
  */
 
-const url = `http://bbs.zhiyoo.net/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1`;
+const url = `http://bbs.zhiyoo.net/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=0&inajax=0&mobile=yes`;
 const method = `POST`;
 const headers = {
 'Connection' : `keep-alive`,
@@ -13,13 +13,13 @@ const headers = {
 'Content-Type' : `application/x-www-form-urlencoded`,
 'Origin' : `http://bbs.zhiyoo.net`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Mobile/15E148 Safari/604.1`,
-'Cookie' : `ikdQ_9242_checkpm=1; ikdQ_9242_connect_is_bind=0; ikdQ_9242_lastact=1612453420%09home.php%09spacecp; ikdQ_9242_noticeTitle=1; ikdQ_9242_sid=fERb0R; ikdQ_9242_mobile=no; ikdQ_9242_sendmail=1; ikdQ_9242_home_diymode=1; ikdQ_9242_nofavfid=1; ikdQ_9242_onlineusernum=1474; ikdQ_9242_auth=f55csaX2kc3Bzq8WFwkUj213egz%2BhKrL1k2BpiE12IES2elfGsaHe%2FiNUv5p9OSQAqEr%2BHOLV9fF5h%2BfjYY%2BJU9EFCs; ikdQ_9242_ulastactivity=9015bmV7y%2F6P%2BahRfMsXEQjw4XLX2Wp4bDXsiHvq3NufKI21S3En; ikdQ_9242_seccode=2017.6c76364a4864a6b67d; ikdQ_9242_lastvisit=1612449565; ikdQ_9242_saltkey=DGU6hhO6`,
+'Cookie' : `ikdQ_9242_connect_is_bind=0; ikdQ_9242_lastact=1613405490%09plugin.php%09; ikdQ_9242_sid=LtT8RM; ikdQ_9242_lip=39.144.29.53%2C1613405401; ikdQ_9242_ulastactivity=8f837RoP69m8zpujf3eTuIeOJwM2IhjujZfwtfYM9AGEBx%2FvuQXY; ikdQ_9242_visitedfid=2; ikdQ_9242_auth=bbd6AaNEdoA3SRBkCqYrqthjr3SwHKLWlxMQ091YNyct0ngybb5GySDPnkgPmlLpYH4V%2FLN5w4pTMeBeayrz8iaWRGE; ikdQ_9242_lastcheckfeed=137509%7C1612495150; ikdQ_9242_nofavfid=1; ikdQ_9242_lastvisit=1612449565; ikdQ_9242_saltkey=DGU6hhO6`,
 'Host' : `bbs.zhiyoo.net`,
 'Referer' : `http://bbs.zhiyoo.net/plugin.php?id=dsu_paulsign:sign`,
 'Accept-Language' : `zh-cn`,
 'Accept' : `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`
 };
-const body = `formhash=cc0a519a`;
+const body = `formhash=cc0a519a&qdxq=kx`;
 
 const myRequest = {
     url: url,
