64adb63e12049b407ec6d220f985bfeaefccd459
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-06-24 21:38:33 +0800

    Update pdags2.js

--- pdags2.js
+++ pdags2.js
@@ -1,14 +1,13 @@
 
-
 /**
  * @fileoverview Template to compose HTTP reqeuest.
  * 
  */
 
-const url = `https://www.pdawiki.com/forum/plugin.php?id=are_yqs_money:time_slot&typeid=2&formhash=3b76551f`;
+const url = `https://www.pdawiki.com/forum/plugin.php?id=are_yqs_money:time_slot&typeid=2&formhash=b4a3f602`;
 const method = `GET`;
 const headers = {
-'Cookie' : `usoK_2132_connect_is_bind=0; usoK_2132_lastact=1611419397%09misc.php%09patch; Hm_lpvt_346381cad752cdb294c1dd3544d1d65a=1611419396; Hm_lvt_346381cad752cdb294c1dd3544d1d65a=1611114577,1611116043,1611159364,1611410162; _ga=GA1.2.1334470196.1610409781; _gid=GA1.2.735512033.1611410164; usoK_2132_noticeTitle=1; usoK_2132_sid=O4hdhx; usoK_2132_sendmail=1; usoK_2132_lip=223.91.28.160%2C1611419331; usoK_2132_onlineusernum=467; usoK_2132_marks=e2d8026358fc3571667d17bf74fe35ec; usoK_2132_ulastactivity=fc64dxGf65kBgcV9n6A3%2BeuXETyrG8eAJqEByoW9kiKA74vZm4ji; usoK_2132_nofavfid=1; usoK_2132_auth=fa07c2amEQWJaoXE2PtjrvJ7KrEozBMLQSPP2rOhMh%2BLBLZZGwH6vYiM6TypS%2F3q1btcQ1nxLZgi%2F%2FgaO3XB6NlXsMw; usoK_2132_lastcheckfeed=335060%7C1611075046; usoK_2132_lastvisit=1611071358; usoK_2132_saltkey=akh19hi2`,
+'Cookie' : `fn_switch_cookie=1; usoK_2132_connect_is_bind=0; usoK_2132_lastact=1624541420%09home.php%09misc; usoK_2132_sendmail=1; usoK_2132_checkpm=1; usoK_2132_sid=NmB0kr; usoK_2132_auth=67d9ErEihlEy48jUgk4o4UK5KerzhDDKU8vOlTDrJ6RGlKJgrvAUvtToOipjnt2ihgncX25q8y2bBpTa%2BUVRES6jRGg; usoK_2132_lastcheckfeed=335060%7C1624540893; usoK_2132_nofavfid=1; usoK_2132_ulastactivity=d80cgDrIyU6Td1slXZcp5Y4YaEsPs5aUYIp%2F0xDFazPUY9WTdQe3; usoK_2132_lastvisit=1623422996; usoK_2132_saltkey=gpf53g19`,
 'Accept' : `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`,
 'Connection' : `keep-alive`,
 'Referer' : `https://www.pdawiki.com/forum/plugin.php?id=are_yqs_money:are_yqs_money`,
