000d600028c3cbaa043fbaea43c0c2cc8fef3744
shouyiyangwencong <36787644+shouyiyangwencong@users.noreply.github.com>
2021-06-24 21:06:27 +0800

    Update pdawiki.js

--- pdawiki.js
+++ pdawiki.js
@@ -10,7 +10,7 @@ const headers = {
 'Accept' : `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`,
 'Origin' : `https://www.pdawiki.com`,
 'Accept-Encoding' : `gzip, deflate, br`,
-'Cookie' : `usoK_2132_connect_is_bind=0; usoK_2132_lastact=1611410176%09misc.php%09patch; Hm_lpvt_346381cad752cdb294c1dd3544d1d65a=1611410176; Hm_lvt_346381cad752cdb294c1dd3544d1d65a=1611114577,1611116043,1611159364,1611410162; _ga=GA1.2.1334470196.1610409781; _gid=GA1.2.735512033.1611410164; usoK_2132_noticeTitle=1; usoK_2132_sid=ErbdER; _gat_gtag_UA_120281465_1=1; usoK_2132_sendmail=1; usoK_2132_lip=223.91.28.160%2C1611410083; usoK_2132_ulastactivity=fc64dxGf65kBgcV9n6A3%2BeuXETyrG8eAJqEByoW9kiKA74vZm4ji; usoK_2132_nofavfid=1; usoK_2132_auth=fa07c2amEQWJaoXE2PtjrvJ7KrEozBMLQSPP2rOhMh%2BLBLZZGwH6vYiM6TypS%2F3q1btcQ1nxLZgi%2F%2FgaO3XB6NlXsMw; usoK_2132_lastcheckfeed=335060%7C1611075046; usoK_2132_lastvisit=1611071358; usoK_2132_saltkey=akh19hi2`,
+'Cookie' : `usoK_2132_connect_is_bind=0; usoK_2132_lastact=1624539508%09home.php%09spacecp; usoK_2132_sid=Fh9yH1; usoK_2132_nofavfid=1; usoK_2132_onlineusernum=548; usoK_2132_auth=3d1cZbmE1%2FdoAmXZG4sIH8po06ga%2BaM5gxXMRdxMQaiGc2%2Fp2xEfztpFiz8aAJYK3iH9abmjGbx23RXfiMuqtep3mgQ; usoK_2132_lastcheckfeed=335060%7C1624539499; usoK_2132_sendmail=1; usoK_2132_ulastactivity=d80cgDrIyU6Td1slXZcp5Y4YaEsPs5aUYIp%2F0xDFazPUY9WTdQe3; usoK_2132_st_p=0%7C1624539450%7Ca33506b877ba5ed1d8efb4444011ae2b; usoK_2132_viewid=tid_29976; usoK_2132_lastvisit=1623422996; usoK_2132_saltkey=gpf53g19`,
 'Content-Type' : `application/x-www-form-urlencoded`,
 'Host' : `www.pdawiki.com`,
 'Connection' : `keep-alive`,
@@ -18,7 +18,7 @@ const headers = {
 'Referer' : `https://www.pdawiki.com/forum/plugin.php?id=dsu_paulsign:sign`,
 'Accept-Language' : `zh-cn`
 };
-const body = `formhash=3b76551f&qdxq=kx&qdmode=3&todaysay=&fastreply=0`;
+const body = `formhash=b4a3f602&qdxq=kx&qdmode=3&todaysay=&fastreply=0`;
 
 const myRequest = {
     url: url,
